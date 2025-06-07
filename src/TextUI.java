49383f1d6778eefa62d653455b18a4cb6ae97969
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-06-07 18:29:14 -0400

    Receive file path input from user and repeat it to console

--- src/TextUI.java
+++ src/TextUI.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class TextUI {
     private final AppService appService;
+    Scanner scanner;
 
     public TextUI(AppService appService) {
         this.appService = appService;
+        this.scanner = new Scanner(System.in);
     }
 
     public void start() {
+        this.inputFilePath();
         this.printCow();
     }
 
     public void printCow() {
+        System.out.println("            ^__^            ");
+        System.out.println("    _______/(oo)            ");
+        System.out.println("/\\/(       /(__)            ");
+        System.out.println("   | w----||                ");
+        System.out.println("   ||     ||                ");
+    }
+
+    public void printCowSaysHello() {
         System.out.println("                    _______ ");
         System.out.println("                   < hello >");
         System.out.println("                    ------- ");
@@ -18,5 +31,11 @@ public class TextUI {
         System.out.println("/\\/(       /(__)            ");
         System.out.println("   | w----||                ");
         System.out.println("   ||     ||                ");
+    }
+
+    public void inputFilePath() {
+        System.out.println("Input the file path:");
+        String input = this.scanner.nextLine();
+        System.out.println(input);
     }
 }
